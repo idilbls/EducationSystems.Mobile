bb574272ced75899b34623dece556ac81ee73010
İdil Bölükbaşı <idilblks@gmail.com>
2021-10-24 15:14:27 +0300

    exit the app added to bottom navigation

--- lib/pages/widget/bottom_navigation_bar.dart
+++ lib/pages/widget/bottom_navigation_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:education_systems_mobile/pages/constants.dart';
+import 'package:flutter/services.dart';
 
 class WelcomeBottomNavigationBar extends StatefulWidget {
   const WelcomeBottomNavigationBar({
@@ -16,22 +17,21 @@ class _WelcomeBottomNavigationBarState
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    // return Row(
-    //   mainAxisAlignment: MainAxisAlignment.center,
-    //   children: [
-    //     Text("Exit",
-    //     style: TextStyle(color: kPrimaryColor, fontSize: 24, fontWeight: FontWeight.bold),),
-    //   ],
-    // );
     return Container(
       height: 60,
       color: Colors.white,
       child: InkWell(
-        onTap: () => print('tap on close'),
+        onTap: (){
+          SystemNavigator.pop();
+        },
         child: Padding(
-          padding: EdgeInsets.only(top: 8.0),
+          padding: EdgeInsets.only(bottom: 10.0, top:10.0),
           child: Column(
             children: <Widget>[
+              Divider(
+                color: kPrimaryColor,
+                height: 10,
+              ),
               Text('Exit',
           style: TextStyle(color: kPrimaryColor, fontSize: 24, fontWeight: FontWeight.bold),),
             ],
