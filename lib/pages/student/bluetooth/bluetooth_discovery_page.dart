013c50da5dd3a9645efce52346591950996c0e71
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2021-12-17 11:06:01 +0300

    fix it bluetooth device connected

--- lib/pages/student/bluetooth/bluetooth_discovery_page.dart
+++ lib/pages/student/bluetooth/bluetooth_discovery_page.dart
@@ -173,7 +173,7 @@ class _BluetoothDiscoveryPageState extends State<BluetoothDiscoveryPage> {
           },
           onLongPress: () async {
             _getUser(buildContext,widget.professorId).then((value)async{
-              if(currentProfessor.localAddress != device.address) {
+              if(currentProfessor.localAddress == device.address) {
                 try {
                   bool bonded = false;
                   if (device.isBonded) {
@@ -224,7 +224,6 @@ class _BluetoothDiscoveryPageState extends State<BluetoothDiscoveryPage> {
                 }
               }
               else{
-                //sadece dersin hocasının cihazına bağlanabilirsiniz!
                 fToast.showToast(
                   child: toast,
                   toastDuration: Duration(seconds: 3),
